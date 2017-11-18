6b529872c6e66dbe16d1bfb79f73081a5225dd48
Özenç Çelik <ozenc.celik@ceng.deu.edu.tr>
2017-11-18 12:44:35 +0300

    Recycler View Ekleniyor

--- app/src/main/java/com/android/zen/agrovita/MainActivity.java
+++ app/src/main/java/com/android/zen/agrovita/MainActivity.java
@@ -2,9 +2,8 @@ package com.android.zen.agrovita;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +11,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public  class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +28,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace" +
+                        " with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -40,6 +42,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+//        Intent intent = new Intent(this, LoginActivity.class);
+//        startActivity(intent);
+
     }
 
     @Override
